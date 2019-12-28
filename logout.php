9d182e1e0412bffff79a263c83473f8c6cb54aa6
QuakeChampions <59294019+QuakeChampions@users.noreply.github.com>
2019-12-28 11:36:41 +0300

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php
+require_once('config.php');
+require_once('includes/dbcon.php');
+require_once('includes/setting.php');
+
+session_unset($GLOBALS['sesId']);
+
+echo '<script>window.location="login_user.php"</script>';
+
+
+
+?>
